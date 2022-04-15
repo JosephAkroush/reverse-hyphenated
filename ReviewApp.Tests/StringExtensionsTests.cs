4fc7a0ae018ef46f61b2866d154759f3c3c4c55f
Joseph Akroush <100246505+JosephAkroush@users.noreply.github.com>
2022-04-14 21:30:40 -0400

    Initial commit of ReviewApp

---
+++ ReviewApp.Tests/StringExtensionsTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReviewApp.Tests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void ReverseHyphenatedLib_WhenNullStringInput_ReturnsNull()
+        {
+            // Arrange
+            string input = null;
+
+            // Act
+            string output = StringExtensions.ReverseHyphenatedLib(input);
+
+            // Assert
+            Assert.IsNull(output);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedLib_WhenEmptyStringInput_ReturnsNull()
+        {
+            // Arrange
+            string input = String.Empty;
+
+            // Act
+            string output = StringExtensions.ReverseHyphenatedLib(input);
+
+            // Assert
+            Assert.IsNull(output);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedLib_WhenWhitespaceStringInput_ReturnsHyphenated()
+        {
+            // Arrange
+            string input = "   ";
+            string expectedOutput = " - - ";
+
+            // Act
+            string actualOutput = StringExtensions.ReverseHyphenatedLib(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedLib_WhenValidStringInput_ReturnsHyphenated()
+        {
+            // Arrange
+            string input = "JOE";
+            string expectedOutput = "E-O-J";
+
+            // Act
+            string actualOutput = StringExtensions.ReverseHyphenatedLib(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedManual_WhenNullStringInput_ReturnsNull()
+        {
+            // Arrange
+            string input = null;
+
+            // Act
+            string output = StringExtensions.ReverseHyphenatedManual(input);
+
+            // Assert
+            Assert.IsNull(output);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedManual_WhenEmptyStringInput_ReturnsNull()
+        {
+            // Arrange
+            string input = String.Empty;
+
+            // Act
+            string output = StringExtensions.ReverseHyphenatedManual(input);
+
+            // Assert
+            Assert.IsNull(output);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedManual_WhenWhitespaceStringInput_ReturnsHyphenated()
+        {
+            // Arrange
+            string input = "   ";
+            string expectedOutput = " - - ";
+
+            // Act
+            string actualOutput = StringExtensions.ReverseHyphenatedManual(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReverseHyphenatedManual_WhenValidStringInput_ReturnsHyphenated()
+        {
+            // Arrange
+            string input = "JOE";
+            string expectedOutput = "E-O-J";
+
+            // Act
+            string actualOutput = StringExtensions.ReverseHyphenatedManual(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
---
+++ ReviewApp/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ReviewApp
+{
+    public static class StringExtensions
+    {
+        public static string ReverseHyphenatedLib(this string value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                return null;
+            }
+
+            return String.Join("-", value.ToCharArray().Reverse());
+        }
+
+        public static string ReverseHyphenatedManual(this string value)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                return null;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = value.Length - 1; i > 0; i--)
+            {
+                stringBuilder.Append(value[i] + "-");
+            }
+
+            return stringBuilder.Append(value[0]).ToString();
+        }
+    }
+}
